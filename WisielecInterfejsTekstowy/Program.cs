88ff8964a3ae976f6be5d160d1f20df0927791c3
kamilm758 <48297624+kamilm758@users.noreply.github.com>
2019-11-02 10:40:06 +0100

    animation+playerName validation

--- WisielecInterfejsTekstowy/Program.cs
+++ WisielecInterfejsTekstowy/Program.cs
@@ -19,7 +19,7 @@ namespace WisielecInterfejsTekstowy
         {
             ": Trudny", ": Łatwy"
         };
-        static string nazwaGracza = "Gal anonim";
+        static string nazwaGracza = "GalAnonim";
         static int aktywnaPozycja = 0;
         static int poziomTrudnosci = 1;
         static void Main(string[] args)
@@ -42,6 +42,14 @@ namespace WisielecInterfejsTekstowy
                     char[] haslo = new char[rozmiarSlowa];
                     char wpisanaLitera;
                     int czyZawiera;
+                    //tabelka boczna
+                    int szerTabelkiBocznej = ("O życie walczy: " + nazwaGracza).Length;
+                    string[] iloscSpacji = new string[szerTabelkiBocznej];
+                    iloscSpacji[0] = " ";
+                    for(int i = 1; i < szerTabelkiBocznej; i++)
+                    {
+                        iloscSpacji[i] = iloscSpacji[i - 1] + " ";
+                    }
                     List<int> pozycjeLiter = new List<int>();
                     for (int i = 0; i < rozmiarSlowa; i++)
                     {
@@ -50,15 +58,20 @@ namespace WisielecInterfejsTekstowy
                     while (true)
                     {
                         Console.Clear();
-                        Console.SetCursorPosition(80,2);
-                        Console.WriteLine("Lifes: " + graWisielec.GetLifes());
-                        Console.SetCursorPosition(80, 4);
-                        Console.WriteLine("Points: " + graWisielec.GetPoints());
-                        Console.SetCursorPosition(80, 6);
-                        Console.WriteLine("O życie walczy: " + nazwaGracza);
-                        Console.SetCursorPosition(0, 0);
-                        Console.WriteLine("Oto hasło, posiada "+rozmiarSlowa+" liter. Haslo o kategorii "+graWisielec.GetSlowo().GetKategoria());
-                        Console.WriteLine("\n\nHasło to: ");
+                        Console.SetCursorPosition(90,2);
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Lifes: " + graWisielec.GetLifes()+iloscSpacji[szerTabelkiBocznej-("Lifes: " + graWisielec.GetLifes()).Length-1]);
+                        Console.SetCursorPosition(90, 3);
+                        Console.Write("Points: " + graWisielec.GetPoints()+iloscSpacji[szerTabelkiBocznej- ("Points: " + graWisielec.GetPoints()).Length-1]);
+                        Console.SetCursorPosition(90, 4);
+                        Console.Write("O życie walczy: " + nazwaGracza);
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.SetCursorPosition(20, 5);
+                        Console.Write("Oto hasło, posiada "+rozmiarSlowa+" liter. Haslo o kategorii "+graWisielec.GetSlowo().GetKategoria());
+                        Console.SetCursorPosition(40, 6);
+                        Console.Write("Hasło to: ");
                         Console.Write(haslo);
                         Console.WriteLine();
                         RysujWisielca(poziomTrudnosci, graWisielec.GetLifes());
@@ -114,6 +127,7 @@ namespace WisielecInterfejsTekstowy
                         catch (Exception) 
                         {
                             Console.WriteLine("Wpisany znak to nie litera! Sprobuj ponownie!");
+                            Console.WriteLine("Wciśnij <<ENTER>> aby kontynuować!");
                         }
                         Console.ReadKey();
                     }
@@ -159,6 +173,7 @@ namespace WisielecInterfejsTekstowy
             Console.WriteLine(@"    \__/  \__/     |__| |_______/    |__| |_______||_______||_______| \______|");
             int x = 10;
             Console.SetCursorPosition(20, x);
+
             for (int i = 0; i < pozycjeMenu.Length; i++)
             {
                 if (i == aktywnaPozycja)
@@ -209,12 +224,23 @@ namespace WisielecInterfejsTekstowy
 
         private static void RysujRanking()
         {
+            int i = 1;// indeksowanie
+            int x = 10;//przechodzenie do kolejnego wiersza w wypisywaniu graczy w rankingu
             Console.Clear();
+            Console.SetCursorPosition(50, 2);
+            Console.Write(RANKING);
+            Console.SetCursorPosition(50, 25);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Wciśnij DOWOLNY PRZYCISK aby powrócić do menu głównego");
+            Console.BackgroundColor = ConsoleColor.Gray;
             Ranking ranking= new Ranking();
             var sortedRanking = ranking.GetSortedRanking();
             foreach (KeyValuePair<string, int> pair in sortedRanking)
             {
-                Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+                Console.SetCursorPosition(55, x);
+                Console.WriteLine(i+": {0}: {1}", pair.Key, pair.Value);
+                i++;
+                x++;
             }
             Console.ReadKey();
         }
@@ -233,8 +259,30 @@ namespace WisielecInterfejsTekstowy
 
         private static void UstawImie()
         {
-            Console.WriteLine("Przedstaw się skazańcze: ");
-            nazwaGracza = Console.ReadLine();
+            do
+            {
+                Console.Clear();
+                Console.SetCursorPosition(50, 13);
+                Console.WriteLine("Przedstaw się skazańcze: ");
+                Console.SetCursorPosition(50, 25);
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("Nie więcej niż 10 liter!");
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.SetCursorPosition(50, 14);
+                nazwaGracza = Console.ReadLine();
+                if (nazwaGracza.Length > 10)
+                {
+                    Console.SetCursorPosition(35, 13);
+                    Console.WriteLine("Nazwa za długa! Spróbuj jeszcze raz!<<PRESS ANY KEY>>");
+                    Console.ReadKey();
+                }
+                else if (nazwaGracza.Length == 0)
+                {
+                    Console.SetCursorPosition(30, 13);
+                    Console.WriteLine("Nazwa musi zawierać przynajmniej jeden znak!<<PRESS ANY KEY>>");
+                    Console.ReadKey();
+                }
+            } while (nazwaGracza.Length > 10 || nazwaGracza.Length==0);
         }
 
         private static void RysujWisielca(int difficulty, int lifes)
@@ -354,5 +402,13 @@ namespace WisielecInterfejsTekstowy
                 Console.WriteLine("        x   x");
             }
         }
+        static readonly string RANKING = @" 
+                                    _____            _   _ _  _______ _   _  _____
+                                   |  __ \     /\   | \ | | |/ /_   _| \ | |/ ____|
+                                   | |__) |   /  \  |  \| | ' /  | | |  \| | |  __ 
+                                   |  _  /   / /\ \ | . ` |  <   | | | . ` | | |_ |
+                                   | | \ \  / ____ \| |\  | . \ _| |_| |\  | |__| |
+                                   |_|  \_\/_/    \_\_| \_|_|\_\_____|_| \_|\_____|
+                                                                                   ";
     }
 }
